9b63d2797f770848cb0280f5e6a17136cdf26730
Aizik_Anonymous <isaac.ogunleye@hotmail.com>
2020-09-21 08:30:06 +0100

    Added utility functions

--- game.py
+++ game.py
@@ -5,3 +5,110 @@ root = tk.Tk
 #set window Title
 root.title("Tic-Tac-Toe")
 
+#value holder variable for player 1
+playera = tk.StringVar()
+
+#value holder variable for player 2
+playerb = tk.StringVar()
+
+p1 = tk.StringVar()
+p2 = tk.StringVar()
+
+#widget for player 1 to enter player name
+player1_name = tk.Entry(root,textvariable=p1, bd=5)
+#place the player1 widget on the window
+player1_name.grid(row=1, column=1, columnspan=8)
+
+#widget for player 2 to enter player name
+player2_name = tk.Entry(root,textvariable=p1, bd=5)
+#place the player2 widget on the window
+player2_name.grid(row=2, column=1, columnspan=8)
+
+#intialize button click to true
+bclick = True
+
+#initialize flag to zero
+flag = 0
+
+buttons = tk.StringVar()
+
+#function for disabling the buttons
+def disableButton():
+    for i in range(1,10):
+        button + i.configure(state="disabled")
+
+#functionality for clicking a button
+def btnClick(buttons):
+    global bclick, flag,player1_name,player2_name,playera,playerb
+    
+    if buttons["text"] == "" and bclick == True:
+        buttons["text"] = "X"
+        bclick = False
+        playerb = p2.get() + "Wins!!"
+        playera = p1.get() + "Wins!!"
+        checkForWin()
+        flag += 1
+    
+    elif buttons["text"] == "" and bclick = False:
+        buttons["text"] = "O"
+        bclick = True
+        checkForWin()
+        flag += 1
+        
+    else:
+        tkinter.messagebox.showinfo("Tic-Tac-Toe", "Button already clicked!!")
+
+#function for checking for winner or draw
+def checkForWin():
+    #check cases where X wins
+    if(
+        button1["text"]=="X" and button2["text"]=="X" and button3["text"]=="X" or
+        button4["text"]=="X" and button5["text"]=="X" and button6["text"]=="X" or
+        button7["text"]=="X" and button8["text"]=="X" and button9["text"]=="X" or
+        button1["text"]=="X" and button4["text"]=="X" and button7["text"]=="X" or
+        button1["text"]=="X" and button5["text"]=="X" and button9["text"]=="X" or
+        button2["text"]=="X" and button5["text"]=="X" and button8["text"]=="X" or
+        button3["text"]=="X" and button6["text"]=="X" and button9["text"]=="X" or
+        button3["text"]=="X" and button5["text"]=="X" and button7["text"]=="X"
+    ):
+        disableButton()
+        tkinter.messagebox.showinfo("Tic-Tac-Toe", playera)
+       
+    # Game ended in tie if flag's count gets to 8 
+    elif flag == 8:
+        tkinter.messagebox.showinfo("Tic-Tac-Toe", "Game ended in a Tie!!")
+      
+    #check for cases where O wins  
+    elif(
+        button1["text"]=="O" and button2["text"]=="O" and button3["text"]=="O" or
+        button4["text"]=="O" and button5["text"]=="O" and button6["text"]=="O" or
+        button7["text"]=="O" and button8["text"]=="O" and button9["text"]=="O" or
+        button1["text"]=="O" and button4["text"]=="O" and button7["text"]=="O" or
+        button1["text"]=="O" and button5["text"]=="O" and button9["text"]=="O" or
+        button2["text"]=="O" and button5["text"]=="O" and button8["text"]=="O" or
+        button3["text"]=="O" and button6["text"]=="O" and button9["text"]=="O" or
+        button3["text"]=="O" and button5["text"]=="O" and button7["text"]=="O"
+    ):
+        disableButton()
+        tkinter.messagebox.showinfo("Tic-Tac-Toe", playerb)
+
+    
+# design labels and buttons
+label = tk.Label(root, text="player 1:", font="Input 17 bold", bg="white", fg="black", height=1,width=8)
+label.grid(row=1, column=0)
+
+label = tk.Label(root, text="player 2:", font="Input 17 bold", bg="white", fg="black", height=1,width=8)
+label.grid(row=2, column=0)
+
+for i in range(1,10):
+    button + i = tk.Button(root,
+                              text="",
+                              font="Input 20 bold",
+                              bg="grey",
+                              fg="white",
+                              height=4,
+                              width=8,
+                              command=lambda : btnClick(button + i))
+    
+    if(i < 4):
+        button+i.grid(row=3, column=0)
